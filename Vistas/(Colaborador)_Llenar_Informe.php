fc620453ec0fdc6adc1312321e42a6893ee413ca
WilsonPI <pujayiglesias@gmail.com>
2020-07-11 12:40:34 -0500

    cambio de tipo de entrada
    input type date

--- Vistas/(Colaborador)_Llenar_Informe.php
+++ Vistas/(Colaborador)_Llenar_Informe.php
@@ -16,20 +16,20 @@ $nombre = ($id_tipo_actividad == 2) ? "Realizadas" : "Planificadas";
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
         <!-- Validate css-->
-        <link href="../CSS/EstiloRegister.css" rel="stylesheet">	
+        <link href="../CSS/EstiloRegister.css" rel="stylesheet">
 
         <!-- Bootstrap CSS -->
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
 
         <!--datables CSS basico-->
         <link rel="stylesheet" type="text/css" href="../datatables/datatables.min.css"/>
-        <!--datables estilo bootstrap 4 CSS-->  
+        <!--datables estilo bootstrap 4 CSS-->
         <link rel="stylesheet"  type="text/css" href="../datatables/DataTables-1.10.18/css/dataTables.bootstrap4.min.css">
         <title>Menú</title>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
 
         <script src="../js/jquery.validate.min.js"></script>
-    </head> 
+    </head>
     <body>
 
         <form name="formRedaccion" id="formInsertar" action="../CONTROLADOR/ColaboradorControlador.php?op=2&tipo_Actividad=<?php echo $id_tipo_actividad;?>" method="post">
@@ -37,7 +37,7 @@ $nombre = ($id_tipo_actividad == 2) ? "Realizadas" : "Planificadas";
             <div style="padding: 30px 200px 100px 200px ; ">
 
                 <div class="modal-header">
-               
+
                     <strong> <h4 style="color: green;"class="modal-title" id="exampleModalLabel">Menu de Redaccion de informes de actividades <?php echo $nombre;?></h4></strong>
                 </div>
                 <div class="modal-body">
@@ -49,11 +49,11 @@ $nombre = ($id_tipo_actividad == 2) ? "Realizadas" : "Planificadas";
 
                     <div class="form-group">
                         <label for="recipient-name" class="col-form-label">Periodo de la actividad(Fecha/Inicio)</label>
-                        <input name="fecha_ini"  type="text" class="form-control" id="fecha_ini">
+                        <input name="fecha_ini"  type="date" class="form-control" id="fecha_ini">
                     </div>
                     <div class="form-group">
                         <label for="recipient-name" class="col-form-label">Periodo de la actividad(Fecha/Final)</label>
-                        <input name="fecha_fin"  type="text" class="form-control" id="fecha_fin">
+                        <input name="fecha_fin"  type="date" class="form-control" id="fecha_fin">
                     </div>
 
                     <div class="form-group">
@@ -69,21 +69,21 @@ $nombre = ($id_tipo_actividad == 2) ? "Realizadas" : "Planificadas";
                 </div>
 
                 <div class="modal-footer">
-                    
-                    <a href="EmpleadoPrincipal.php" class="btn btn-secondary" >Volver</a>              
+
+                    <a href="EmpleadoPrincipal.php" class="btn btn-secondary" >Volver</a>
                     <input type="submit" value="Siguiente" id="boton" class="btn btn-info">
-                    
+
                 </div>
             </div>
 
-        </form> 
+        </form>
 
     </body>
 
     <!-- datatables JS -->
-    <script type="text/javascript" src="../datatables/datatables.min.js"></script>    
+    <script type="text/javascript" src="../datatables/datatables.min.js"></script>
 
-    <script type="text/javascript" src="../jquery/main.js"></script>  
+    <script type="text/javascript" src="../jquery/main.js"></script>
 
 </body>
 </html>
