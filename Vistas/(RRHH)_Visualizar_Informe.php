eae90affbbdd9e310edbf4ac98fb4b98a41830e0
Elmer <sofnito@gmail.com>
2020-07-12 15:36:48 -0500

    Commit
    Recorrido de detalle de informe para evitar errores

--- Vistas/(RRHH)_Visualizar_Informe.php
+++ Vistas/(RRHH)_Visualizar_Informe.php
@@ -51,29 +51,41 @@ var_dump($detalleInforme);
             </div>
             <!-- TIENES PENSADO AVANZAR LA VISUALIZACION DE LOS INFORMES , ASI COMO EL MINI MENU DE ENVIO A RR.HH Y EL SPRITN ESTARA TERMINADO -->
             <!--Ejemplo tabla con DataTables-->
+            <?php
+            $s=0;
+            foreach($detalleInforme as $det):?>
+            
             <div style="max-width: 1350px;" class="container">
                 <div class="row">
                     <div class="col-lg-12">
+                        <?php if(!empty($detalleInforme[$s]['titulo_desc'])){ ?>
                         <div class="form-group">
                             <label  style="font-weight: bold">Titulo General :</label>
                             <p> 
-                                <?php echo $detalleInforme[1]['titulo_desc']; ?> 
+                                <?php echo $detalleInforme[$s]['titulo_desc'];?> 
                             </p>
                         </div>
+                        <?php } ?>
                         
+                        <?php if(!empty($detalleInforme[$s]['asunto_det'])){?>
                         <div class="form-group">
                             <label  style="font-weight: bold">Asunto :</label>
                             <p> 
-                                <?php echo $detalleInforme[1]['asunto_det']; ?> 
+                                <?php echo $detalleInforme[$s]['asunto_det']; ?> 
                             </p>
                         </div>
+                        <?php }?>
                         
+                        <?php if(!empty($detalleInforme[$s]['desc_det'])) {?>
                         <div class="form-group">
                             <label  style="font-weight: bold">Descripcion del Detalle :</label>
                             <p> 
-                                <?php echo $detalleInforme[1]['desc_det']; ?> 
+                                <?php echo $detalleInforme[$s]['desc_det']; ?> 
                             </p>
                         </div>
+                        <?php } ?>
+                        
+                        <?php $s++; endforeach;?>
                         
                         <div class="form-group">
                             <label  style="font-weight: bold">Titulo de Informe :</label>
